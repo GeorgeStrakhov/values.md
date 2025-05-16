3c68a0e9368e3409b811e804bf1794a72c73a199
gs <george.strakhov@gmail.com>
2025-05-16 21:13:52 +0200

    init empty

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxt/icon',
+    '@nuxt/image',
+    '@nuxt/scripts',
+    '@nuxt/ui',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+})
